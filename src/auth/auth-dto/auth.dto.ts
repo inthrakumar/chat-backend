14ad2c63af901b712892853147ecddc20ed315fc
inthrakumar <inthrak04@gmail.com>
2024-10-26 19:09:58 +0530

    change in dto for password

--- src/auth/auth-dto/auth.dto.ts
+++ src/auth/auth-dto/auth.dto.ts
@@ -1,10 +1,10 @@
 import {
   IsString,
   IsEmail,
-  IsAlphanumeric,
+  IsNotEmpty,
+  IsStrongPassword,
   MinLength,
   MaxLength,
-  IsNotEmpty,
 } from 'class-validator';
 
 export class AuthDTO {
@@ -18,16 +18,23 @@ export class AuthDTO {
     message: 'Username must not exceed 20 characters.',
   })
   username: string;
-  @IsString()
+
   @IsNotEmpty()
-  @MinLength(8, {
-    message:
-      'Username must be at least 10 characters long and contain only alphanumeric characters.',
-  })
-  @MaxLength(20, {
-    message: 'Username must not exceed 20 characters.',
-  })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minUppercase: 1,
+      minLowercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    },
+  )
   password: string;
+
   @IsEmail()
   @IsNotEmpty()
   email: string;
